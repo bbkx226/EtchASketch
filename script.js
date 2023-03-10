e68baf3120a5d86d06fa0bbb820a3facbe031c90
Brandon <bbkx226@gmail.com>
2023-03-10 15:37:24 +0800

    Implement all functions

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let color = "black"
+let click = false;
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if (e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw")
+            if(click){
+                draw.innerHTML = "Now You Can Draw. Click Again to stop Drawing";
+            } else {
+                draw.innerHTML = "You're Not Allowed To Draw. Click to Start/Continue Drawing"
+            }
+        }
+    })
+
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
         let size = getSize();
@@ -34,10 +49,12 @@ function getSize(){
 }
 
 function colorDiv() {
-    if (color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    } else {
-        this.style.backgroundColor = 'black'
+    if(click){
+        if (color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        } else {
+            this.style.backgroundColor = 'black'
+        }
     }
 }
 
